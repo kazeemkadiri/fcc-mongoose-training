eaa17c0af00b5ecd39d35d5bdf648f41c44c19b6
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-16 22:03:13 +0100

    Added connection code for mongoose in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var mongoose = require("mongoose");
-
-momgoose.connect(process.env.MONGO_URI);
+     
+mongoose.connect("mongodb://127.0.0.1/my_database:27107" || process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology:true});
+                 
+console.log(process.env.MONGO_URI);
