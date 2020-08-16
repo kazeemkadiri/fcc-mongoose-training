4781088455ba4cb020f30e6a767a436413ae2590
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-16 22:38:10 +0100

    Imported the person schema into index.js and created a model from it

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var mongoose = require("mongoose");
-     
+
+var person = require("./models/person_schema");
+   
 mongoose.connect("mongodb://127.0.0.1/my_database:27107" || process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology:true});
                  
-console.log(process.env.MONGO_URI);
+//console.log(process.env.MONGO_URI);
+const model = mongoose.model("Person",person);
+                           
+const document = new model({name:"kk", age:23, favoriteFoods:["rice"]})
+
+document.save(function(err, data){
+  console.log(data);
+});
