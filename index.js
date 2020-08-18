4d43c6a83564953f348dbd592e3c5d9fd5b3adb4
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-18 09:56:13 +0100

    Added code to search for a person using the find method on the model

--- index.js
+++ index.js
@@ -8,20 +8,28 @@ mongoose.connect("mongodb://127.0.0.1/my_database:27017"
 //console.log(process.env.MONGO_URI);
 const model = mongoose.model("Person",person);
 //create a new document                           
-const document = new model({name:"kk", age:23, favoriteFoods:["rice"]})
+//const document = new model({name:"kk", age:23, favoriteFoods:["rice"]})
 
 //Save the document
-document.save(function(err, data){
+/*document.save(function(err, data){
   console.log(data);
-});
+});*/
 
 //An array of people 
-const arrayOfPeople =
+/*const arrayOfPeople =
  [{name:"wale",age:"22"},
   {name:"qasim", age:"18"}];
-
+*/
 //Insert array of people into database
-model.create(arrayOfPeople, function(err, people){
+/*model.create(arrayOfPeople, function(err, people){
   if(err){ console.log(err); }
   console.log(people);
+});
+*/
+
+//Search for a document by passing in an argument into the find method
+const personName = {name: "qasim"};
+model.find(personName, function(err, document){
+  if(err) console.log(err);
+  console.log(document);
 });
